1a8a357807e760a6a5bd51b4f94ea6f1f76f0ebb
John Hui <jwongsquared@gmail.com>
2017-05-31 23:19:59 -0700

    more error checking

--- infect.sh
+++ infect.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+TAG="[POKERUS]"
+
 if [[ "$#" -lt 1 ]]; then
 	echo "usage: $0 [<boxes> ...]"
 	exit -1
 fi
 
 for dir in "$@"; do
-	stow "$dir" --target="$HOME" --ignore='\.DS_Store'
+	if [[ -d "$dir" ]]; then
+		stow "$dir" --target="$HOME" --ignore='\.DS_Store'
+		echo "$TAG infected $HOME with $dir"
+	else
+		echo "$TAG $dir is not a directory"
+	fi
 done
