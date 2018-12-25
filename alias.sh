4d6c718906d0c2d6962b42a10f2ba1b8a97e3412
John Hui <jwongsquared@gmail.com>
2018-12-26 06:20:07 +0800

    added emacs initialization

--- alias.sh
+++ alias.sh
@@ -14,12 +14,14 @@ echo "$TAG aliasing to using ~/$profile"
 
 src='
 if [ -f ~/.bash_aliases ]; then
-	. ~/.bash_aliases
+	. ~/.bash_aliases # Pokerus
 fi
 '
 
-if grep -q '. ~/.bash_aliases' ~/$profile; then
-	echo "$TAG error: ~/$profile seems already aliased"
+if grep -q '~/.bash_aliases' ~/$profile; then
+	echo "$TAG error: ~/$profile seems already aliased:"
+    echo
+    grep -C 3 '~/.bash_aliases' ~/$profile
 	exit -1
 else
 	echo "$src" >> ~/$profile
