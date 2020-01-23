7c32c51ceec3533c8436fb15563bde8187922ecd
j-hui <j-hui@cs.columbia.edu>
2020-01-22 23:15:45 -0500

    config files for kitty, yabai, skhdrc

--- infect.sh
+++ infect.sh
@@ -10,8 +10,14 @@ fi
 for dir in "$@"; do
 	if [[ ! -d "$dir" ]]; then
 		echo "$TAG $dir is not a directory"
-	elif [[ "${dir##*.}" == "immune" ]]; then
+	elif [[ "${dir##*.}" == "immune" || "${dir##*.}" == "immune/" ]]; then
 		echo "$TAG ignoring $dir"
+	elif [[ "${dir##*.}" == "config" || "${dir##*.}" == "config/" ]]; then
+		if stow -vv "$dir" --target="$HOME/.config" --ignore='\.DS_Store'; then
+			echo "$TAG infected $HOME/.config with $dir"
+		else
+			echo "$TAG encountered error infecting $dir"
+		fi
 	else
 		if stow -vv "$dir" --target="$HOME" --ignore='\.DS_Store'; then
 			echo "$TAG infected $HOME with $dir"
