fdefa05cbeb97663b0eb6325fc8c953f1ee06d3b
j-hui <j-hui@cs.columbia.edu>
2020-06-09 16:28:09 -0400

    infect script

--- infect.sh
+++ infect.sh
@@ -1,28 +1,87 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+set -e
 TAG="[POKERUS]"
 
-if [[ "$#" -lt 1 ]]; then
+function pecho () { echo "$TAG           " "$@" >&2 ; }
+function wecho () { echo "$TAG  WARN     " "$@" >&2 ; }
+function eecho () { echo "$TAG  ERROR    " "$@" >&2 ; }
+
+# Echoes "." if we want to ignore it
+function ignores () {
+    case "$1" in
+        .DS_STORE)
+            echo .
+            ;;
+        *~|*.swp)
+            echo .
+            ;;
+        ..)
+            echo .
+            ;;
+        *)
+            echo "$1"
+            ;;
+    esac
+}
+
+function infect () {
+    src="$1"
+    dst="$2"
+
+	if ! [ -d "$src" ]; then
+		wecho "Source $src is not a directory"
+        return
+    fi
+
+    pecho "Infecting $dst with contents of $src..."
+
+    if ! [ -d "$dst" ]; then
+		pecho "Destination $dst does not yet existing, creating now..."
+        mkdir -p "$dst"
+    fi
+
+    ls -1 -a "$src" | while read -r f ; do
+        # Skip ignored items
+        case "$(ignores $f)" in .|..|"") continue ;; esac
+
+        if [ -h "$dst/$f" ]; then
+            # Some symbolic link already exists at dst
+            if [ "$(readlink -e "$dst/$f")" = "$(readlink -e "$src/$f")" ]; then
+                wecho "Already symlinked: $dst/$f -> $src/$f. Skipping."
+            else
+                eecho "A symlink already exists at $dst/$f. Skipping."
+                eecho "  $dst/$f -> $(readlink "$dst/$f")"
+            fi
+        elif [ -e "$dst/$f" ]; then
+            eecho "A file already exists at $dst/$f. Skipping."
+        else
+            # We're good to go, symlink away
+            pecho "Symlinking $src/$f..."
+            ln -s "$(realpath --relative-to="$dst" "$src/$f")" "$dst/$f"
+        fi
+    done
+}
+
+if [ "$#" -lt 1 ]; then
 	echo "usage: $0 [<boxes> ...]"
 	exit -1
 fi
 
 for dir in "$@"; do
-	if [[ ! -d "$dir" ]]; then
-		echo "$TAG $dir is not a directory"
-	elif [[ "${dir##*.}" == "immune" || "${dir##*.}" == "immune/" ]]; then
-		echo "$TAG ignoring $dir"
-	elif [[ "${dir##*.}" == "config" || "${dir##*.}" == "config/" ]]; then
-		if stow -vv "$dir" --target="$HOME/.config" --ignore='\.DS_Store'; then
-			echo "$TAG infected $HOME/.config with $dir"
-		else
-			echo "$TAG encountered error infecting $dir"
-		fi
-	else
-		if stow -vv "$dir" --target="$HOME" --ignore='\.DS_Store'; then
-			echo "$TAG infected $HOME with $dir"
-		else
-			echo "$TAG encountered error infecting $dir"
-		fi
-	fi
+    dir="$(realpath "$dir")"
+
+    pecho "Infecting $dir..."
+
+    case "$dir" in
+        *.immune)
+            pecho "ignoring $dir"
+            ;;
+        *.config)
+            infect "$dir" "$HOME/.config"
+            ;;
+        *)
+            infect "$dir" "$HOME"
+            ;;
+    esac
 done
