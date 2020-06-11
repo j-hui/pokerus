d0ff625ae5355740dde469e4b6ba0289f4de8b1d
J-Hui <j-hui@cs.columbia.edu>
2020-06-11 18:56:37 -0400

    deleted old emacs

--- emacs.deprecated/.doom.d/packages.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; .doom.d/packages.el
-
-;;; Examples:
-
-;; (package! proof-general)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
