ee85e1e2b64a42af348d664af227709d4e2d8c71
j-hui <j-hui@cs.columbia.edu>
2021-01-03 16:08:29 -0500

    added bluetooth stuffs

--- nixos.config/nixos/console.nix
+++ nixos.config/nixos/console.nix
@@ -106,6 +106,8 @@ in
     })
 
     (mkIf cfg.laptop.enable {
+      networking.networkmanager.enable = true;
+      hardware.bluetooth.enable = true;
       environment.systemPackages = with pkgs; [
         brightnessctl
       ];
