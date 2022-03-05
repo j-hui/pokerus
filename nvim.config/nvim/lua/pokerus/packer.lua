3fa408f0e2e8206f1bb377d4ccbe346c8bcf2732
j-hui <j-hui@cs.columbia.edu>
2022-03-05 15:05:00 -0500

    [nvim]: Switch to packer.nvim (wip)

--- nvim.config/nvim/lua/pokerus/packer.lua
+++ nvim.config/nvim/lua/pokerus/packer.lua
@@ -18,54 +18,15 @@ vim.cmd [[packadd packer.nvim]]
 
 return require("packer").startup {
   function(use)
+    -- "Use" packer so that it doesn't get marked for deletion
     use { "wbthomason/packer.nvim", opt = true }
-    require("pokerus.plugins.alpha").plug(use)
-    require("pokerus.plugins.bufferline").plug(use)
-    require("pokerus.plugins.close-pair").plug(use)
-    require("pokerus.plugins.cmp").plug(use)
-    require("pokerus.plugins.comment").plug(use)
-    require("pokerus.plugins.coqtail").plug(use)
-    require("pokerus.plugins.css-color").plug(use)
-    require("pokerus.plugins.dressing").plug(use)
-    require("pokerus.plugins.fidget").plug(use)
-    require("pokerus.plugins.fugitive").plug(use)
-    require("pokerus.plugins.gitlinker").plug(use)
-    require("pokerus.plugins.gitsigns").plug(use)
-    require("pokerus.plugins.goyo").plug(use)
-    require("pokerus.plugins.hls").plug(use)
-    require("pokerus.plugins.illuminate").plug(use)
-    require("pokerus.plugins.interestingwords").plug(use)
-    require("pokerus.plugins.latex").plug(use)
-    require("pokerus.plugins.lightbulb").plug(use)
-    require("pokerus.plugins.lightspeed").plug(use)
-    require("pokerus.plugins.lir").plug(use)
-    require("pokerus.plugins.lsp").plug(use)
-    require("pokerus.plugins.lualine").plug(use)
-    require("pokerus.plugins.markdown").plug(use)
-    require("pokerus.plugins.marks").plug(use)
-    require("pokerus.plugins.neogen").plug(use)
-    require("pokerus.plugins.null-ls").plug(use)
-    require("pokerus.plugins.numb").plug(use)
-    require("pokerus.plugins.octo").plug(use)
-    require("pokerus.plugins.project").plug(use)
-    require("pokerus.plugins.scrollbar").plug(use)
-    require("pokerus.plugins.sideways").plug(use)
-    require("pokerus.plugins.stabilize").plug(use)
-    require("pokerus.plugins.stickybuf").plug(use)
-    require("pokerus.plugins.telescope").plug(use)
-    require("pokerus.plugins.textobj").plug(use)
-    require("pokerus.plugins.todo-comments").plug(use)
-    require("pokerus.plugins.tokyonight").plug(use)
-    require("pokerus.plugins.treesitter").plug(use)
-    require("pokerus.plugins.trouble").plug(use)
-    require("pokerus.plugins.twilight").plug(use)
-    require("pokerus.plugins.venn").plug(use)
-    require("pokerus.plugins.vimbridge").plug(use)
-    require("pokerus.plugins.virt-column").plug(use)
-    require("pokerus.plugins.vsnip").plug(use)
-    require("pokerus.plugins.which-key").plug(use)
-    require("pokerus.plugins.zk").plug(use)
-    use "dstein64/vim-startuptime"
+
+    -- Automatically require any plugin module in ~/.config/nvim/lua/pokerus/plugins/
+    for _, p in ipairs(fn.split(fn.glob((fn.stdpath "config") .. "/lua/pokerus/plugins/*"))) do
+      p = fn.substitute(p, "^.*/", "", "")
+      p = fn.substitute(p, "\\.lua$", "", "")
+      require("pokerus.plugins." .. p).plug(use)
+    end
 
     if packer_bootstrap then
       -- Automatically set up configuration after cloning packer.nvim
