b7bb77063df87cb8d2078e6490a59f2dad3564a1
j-hui <j-hui@cs.columbia.edu>
2022-03-16 01:11:32 -0400

    [nvim]: Fix bindings for lightspeed

--- nvim.config/nvim/lua/pokerus/plugins/lightspeed.lua
+++ nvim.config/nvim/lua/pokerus/plugins/lightspeed.lua
@@ -1,5 +1,6 @@
 return {
   plug = function(use)
+    vim.g.lightspeed_no_default_keymaps = true
     use {
       "ggandor/lightspeed.nvim",
       opt = true,
@@ -9,18 +10,30 @@ return {
           exit_after_idle_msecs = { labeled = 2500, unlabled = 1000 },
           limit_ft_matches = 16,
         }
-        require("pokerus").xmap {
-          s = "<Plug>Lightspeed_s",
-          S = "<Plug>Lightspeed_S",
-        }
-        require("pokerus").omap {
-          c = "<Plug>Lightspeed_s",
-          C = "<Plug>Lightspeed_S",
-        }
+
         require("pokerus").nmap {
-          [","] = "<Plug>Lightspeed_;_ft",
-          ["[,"] = "<Plug>Lightspeed_;_ft",
-          ["],"] = "<Plug>Lightspeed_,_ft",
+          ["f"] = { "<Plug>Lightspeed_f", "lightspeed-f" },
+          ["F"] = { "<Plug>Lightspeed_F", "lightspeed-F" },
+          ["t"] = { "<Plug>Lightspeed_t", "lightspeed-t" },
+          ["T"] = { "<Plug>Lightspeed_T", "lightspeed-T" },
+          ["s"] = { "<Plug>Lightspeed_s", "lightspeed-s" },
+          ["S"] = { "<Plug>Lightspeed_S", "lightspeed-S" },
+          ["g/"] = { "<Plug>Lightspeed_omni_s", "lightspeed-omni-s" },
+          [","] = { "<Plug>Lightspeed_;_ft", "lightspeed-;" },
+          ["[,"] = { "<Plug>Lightspeed_;_ft", "lightspeed-;" },
+          ["],"] = { "<Plug>Lightspeed_,_ft", "lightspeed-;" },
+        }
+
+        require("pokerus").vmap {
+          c = { "<Plug>Lightspeed_s", "lightspeed-s" },
+          C = { "<Plug>Lightspeed_S", "lightspeed-S" },
+        }
+
+        require("pokerus").omap {
+          c = { "<Plug>Lightspeed_s", "lightspeed-s" },
+          C = { "<Plug>Lightspeed_S", "lightspeed-S" },
+          x = { "<Plug>Lightspeed_x", "lightspeed-x" },
+          X = { "<Plug>Lightspeed_X", "lightspeed-X" },
         }
       end,
     }
