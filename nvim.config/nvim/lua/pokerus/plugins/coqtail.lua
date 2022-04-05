2fef831939eb1ecb754520e8acc404dc31c5b96e
j-hui <j-hui@cs.columbia.edu>
2022-04-05 15:27:05 -0400

    [nvim]: Fix coqtail bindings

--- nvim.config/nvim/lua/pokerus/plugins/coqtail.lua
+++ nvim.config/nvim/lua/pokerus/plugins/coqtail.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.maps()
   require("pokerus").nmap({
-    c = {
+    l = {
       name = "coqtail",
       Q = { "<Plug>CoqStart", "coq-start" },
       q = { "<Plug>CoqStop", "coq-stop" },
@@ -29,7 +29,7 @@ function M.maps()
   }, { prefix = "<leader>", buffer = 0, noremap = false })
 
   require("pokerus").xmap({
-    c = {
+    l = {
       name = "coqtail",
       j = { "<Plug>CoqCheck", "coq-check" },
       k = { "<Plug>CoqAbout", "coq-about" },
@@ -40,8 +40,19 @@ function M.maps()
   }, { prefix = "<leader>", buffer = 0, noremap = false })
 
   require("pokerus").imap({
+    n = { "<Esc><Plug>CoqNext", "coq-next" },
+    p = { "<Esc><Plug>CoqUndo", "coq-undo" },
     l = { "<Esc><Plug>CoqToLine", "coq-to-line" },
     c = { "<Esc>mz$<Plug>CoqToLine`z", "coq-to-eol" },
+    ["<CR>"] = { "<Esc>mz$<Plug>CoqToLine`z", "coq-to-eol" },
+  }, { prefix = "<C-c>", buffer = 0, noremap = false })
+
+  require("pokerus").nmap({
+    n = { "<Plug>CoqNext", "coq-next" },
+    p = { "<Plug>CoqUndo", "coq-undo" },
+    l = { "<Plug>CoqToLine", "coq-to-line" },
+    c = { "mz$<Plug>CoqToLine`z", "coq-to-eol" },
+    ["<CR>"] = { "mz$<Plug>CoqToLine`z", "coq-to-eol" },
   }, { prefix = "<C-c>", buffer = 0, noremap = false })
 
   require("pokerus").nmap {
@@ -56,26 +67,37 @@ function M.maps()
   }
 end
 
+function M.highlights()
+  -- hard-coded colors come from tokyonight DiffChange and DiffAdd
+  vim.cmd [[
+    highlight CoqtailChecked ctermbg=236 guibg=#1f2231
+    highlight CoqtailSent    ctermbg=237 guibg=#20303b
+  ]]
+end
+
+function M.config()
+  vim.g.coqtail_match_shift = 1
+  vim.g.coqtail_indent_on_dot = 1
+  vim.g.coqtail_auto_set_proof_diffs = "on"
+  vim.g.coqtail_update_tagstack = 1
+  vim.g.coqtail_nomap = 1
+
+  vim.cmd [[
+    augroup coqtail_cmds
+      autocmd!
+      autocmd ColorScheme * lua require("pokerus.plugins.coqtail").highlights()
+      autocmd Filetype coq,coq-infos,coq-goals lua require("pokerus.plugins.coqtail").maps()
+      autocmd Filetype coq syntax sync fromstart
+    augroup END
+  ]]
+  require("pokerus.plugins.coqtail").highlights()
+end
+
 function M.plug(use)
   use {
     "whonore/coqtail",
     config = function()
-      vim.g.coqtail_match_shift = 1
-      vim.g.coqtail_indent_on_dot = 1
-      vim.g.coqtail_auto_set_proof_diffs = "on"
-      vim.g.coqtail_update_tagstack = 1
-      vim.g.coqtail_nomap = 1
-
-      vim.cmd [[
-        augroup coqtail_commands
-          autocmd!
-          autocmd ColorScheme *
-                \   highlight def CoqtailChecked ctermbg=236
-                \|  highlight def CoqtailSent    ctermbg=237
-          autocmd Filetype coq,coq-infos,coq-goals lua require("coqtail").maps()
-          autocmd Filetype coq syntax sync fromstart
-        augroup END
-      ]]
+      require("pokerus.plugins.coqtail").config()
     end,
   }
 end
