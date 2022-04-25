04a38723461338dc7c06fa626a553f241fa06a7f
j-hui <j-hui@cs.columbia.edu>
2022-04-25 11:01:39 -0400

    [neovim]: don't rely on closure for lightbulb setup

--- nvim.config/nvim/lua/pokerus/plugins/lightbulb.lua
+++ nvim.config/nvim/lua/pokerus/plugins/lightbulb.lua
@@ -1,18 +1,32 @@
-return {
-  plug = function(use)
-    use {
-      "kosayoda/nvim-lightbulb",
-      -- Make code actions more visible
-      requires = { "neovim/nvim-lspconfig" },
-      config = function()
-        vim.cmd [[
-          sign define LightBulbSign text=↯ texthl=SignColumn
-          augroup NvimLightbulb
-            autocmd!
-            autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()
-          augroup END
-        ]]
-      end,
-    }
-  end,
-}
+local M = {}
+
+function M.config()
+  vim.fn.sign_define(
+    "LightBulbSign",
+    { text = "↯", texthl = "SignColumn", linehl = "", numhl = "" }
+  )
+
+  require("nvim-lightbulb").setup {
+    ignore = { "null-ls" },
+  }
+
+  vim.cmd [[
+    augroup NvimLightbulb
+      autocmd!
+      autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()
+    augroup END
+  ]]
+end
+
+function M.plug(use)
+  use {
+    "kosayoda/nvim-lightbulb",
+    -- Make code actions more visible
+    requires = { "neovim/nvim-lspconfig" },
+    config = function()
+      require("pokerus.plugins.lightbulb").config()
+    end,
+  }
+end
+
+return M
