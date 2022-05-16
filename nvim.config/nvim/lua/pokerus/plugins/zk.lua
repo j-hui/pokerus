6029cfb61f26c53151bedae24d8fa525d21b2049
j-hui <j-hui@cs.columbia.edu>
2022-05-16 11:59:59 -0400

    [nvim]: proper setup pattern for zk

--- nvim.config/nvim/lua/pokerus/plugins/zk.lua
+++ nvim.config/nvim/lua/pokerus/plugins/zk.lua
@@ -1,12 +1,18 @@
-return {
-  plug = function(use)
-    use {
-      "mickael-menu/zk-nvim",
-      opt = true,
-      event = "VimEnter",
-      config = function()
-        require("zk").setup {}
-      end,
-    }
-  end,
-}
+local M = {}
+
+function M.config()
+  require("zk").setup {}
+end
+
+function M.plug(use)
+  use {
+    "mickael-menu/zk-nvim",
+    opt = true,
+    event = "VimEnter",
+    config = function()
+      require("pokerus.plugins.zk").config()
+    end,
+  }
+end
+
+return M
