2c688b4b07fece1187d006de6e1ac2b3747bd7dc
j-hui <j-hui@cs.columbia.edu>
2022-05-27 00:41:21 -0400

    [zsh]: Ignore notifying about some more interactive applications

--- zsh.config/zsh/plugins/auto-notify.zsh
+++ zsh.config/zsh/plugins/auto-notify.zsh
@@ -2,4 +2,6 @@
 
 zinit light MichaelAquilina/zsh-auto-notify
 
-AUTO_NOTIFY_IGNORE+=("config" "zsh" "fish")
+AUTO_NOTIFY_IGNORE+=("config")
+AUTO_NOTIFY_IGNORE+=("zsh" "fish")
+AUTO_NOTIFY_IGNORE+=("e" "edit" "vim" "nvim" "vi" "emacs")
