dd96b891df834ad08097daa20b0bf3bffbc0c776
j-hui <j-hui@cs.columbia.edu>
2022-06-13 23:36:08 -0400

    [nvim]: Fix interesting words binding

--- nvim.config/nvim/lua/pokerus/plugins/interestingwords.lua
+++ nvim.config/nvim/lua/pokerus/plugins/interestingwords.lua
@@ -1,46 +1,53 @@
-return {
-  plug = function(use)
-    use {
-      "lfv89/vim-interestingwords",
-      config = function()
-        vim.g.interestingWordsDefaultMappings = 0
+local M = {}
 
-        require("pokerus").nmap {
-          ["<leader>i"] = {
-            function()
-              vim.fn.InterestingWords "n"
-            end,
-            "mark-interesting",
-          },
-          ["<leader>I"] = {
-            function()
-              vim.cmd [[noh]]
-              vim.fn.UncolorAllWords()
-            end,
-            "unmark-all",
-          },
-          ["]i"] = {
-            function()
-              vim.fn.WordNavigation(1)
-            end,
-            "next-interesting",
-          },
-          ["[i"] = {
-            function()
-              vim.fn.WordNavigation(0)
-            end,
-            "prev-interesting",
-          },
-        }
+function M.config()
+  vim.g.interestingWordsDefaultMappings = 0
 
-        require("pokerus").xmap {
-          ["<leader>i"] = {
-            function()
-              vim.fn.InterestingWords "n"
-            end,
-          },
-        }
+  require("pokerus").nmap {
+    ["<leader>i"] = {
+      function()
+        vim.fn.InterestingWords "n"
       end,
-    }
-  end,
-}
+      "mark-interesting",
+    },
+    ["<leader>I"] = {
+      function()
+        vim.cmd [[noh]]
+        vim.fn.UncolorAllWords()
+      end,
+      "unmark-all",
+    },
+    ["]i"] = {
+      function()
+        vim.fn.WordNavigation(1)
+      end,
+      "next-interesting",
+    },
+    ["[i"] = {
+      function()
+        vim.fn.WordNavigation(0)
+      end,
+      "prev-interesting",
+    },
+  }
+
+  require("pokerus").xmap {
+    ["<leader>i"] = {
+      function()
+        vim.fn.InterestingWords "n"
+      end,
+      "mark-interesting",
+    },
+  }
+end
+
+function M.plug(use)
+  use {
+    "lfv89/vim-interestingwords",
+    config = function()
+      require("pokerus.plugins.interestingwords").config()
+    end,
+  }
+end
+
+return M
