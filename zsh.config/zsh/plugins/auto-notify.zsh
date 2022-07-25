ab45fcdbb0075c484c8995aa2f4d018ff1ade8eb
j-hui <j-hui@cs.columbia.edu>
2022-07-25 13:38:29 -0400

    [zsh]: only use auto-notify if notify-send is present

--- zsh.config/zsh/plugins/auto-notify.zsh
+++ zsh.config/zsh/plugins/auto-notify.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
-zinit light MichaelAquilina/zsh-auto-notify
+if which notify-send >/dev/null ; then
+    zinit light MichaelAquilina/zsh-auto-notify
 
-AUTO_NOTIFY_IGNORE+=("config")
-AUTO_NOTIFY_IGNORE+=("zsh" "fish")
-AUTO_NOTIFY_IGNORE+=("e" "edit" "vim" "nvim" "vi" "emacs")
+    AUTO_NOTIFY_IGNORE+=("config")
+    AUTO_NOTIFY_IGNORE+=("zsh" "fish")
+    AUTO_NOTIFY_IGNORE+=("e" "edit" "vim" "nvim" "vi" "emacs")
+fi
