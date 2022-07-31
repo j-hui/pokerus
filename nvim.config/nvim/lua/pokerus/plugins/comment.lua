d29152b655b9456859be312c006eee556750b144
j-hui <j-hui@cs.columbia.edu>
2022-07-31 01:21:59 -0400

    [nvim]: Add happy and alex comment styles to Comment.nvim

--- nvim.config/nvim/lua/pokerus/plugins/comment.lua
+++ nvim.config/nvim/lua/pokerus/plugins/comment.lua
@@ -1,16 +1,26 @@
-return {
-  plug = function(use)
-    use {
-      "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup {}
-        require("pokerus").nmap {
-          g = {
-            c = "line-comment",
-            b = "block-comment",
-          }
-        }
-      end,
-    }
-  end,
-}
+local M = {}
+
+function M.config()
+  require("Comment").setup {}
+
+  local ft = require('Comment.ft')
+  ft({'alex', 'happy'}, {'--%s', '{-%s-}'})
+
+  require("pokerus").nmap {
+    g = {
+      c = "line-comment",
+      b = "block-comment",
+    },
+  }
+end
+
+function M.plug(use)
+  use {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("pokerus.plugins.comment").config()
+    end,
+  }
+end
+
+return M
