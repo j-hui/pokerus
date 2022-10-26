79b36e955fbdd8695eff30cfa99cd07408dadc18
John Hui <j-hui@cs.columbia.edu>
2022-10-26 00:48:19 -0400

    [zsh] Get rid of auto-notify plugin

--- zsh.config/zsh/plugins/auto-notify.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-if which notify-send >/dev/null ; then
-    zinit light MichaelAquilina/zsh-auto-notify
-
-    AUTO_NOTIFY_IGNORE+=("config")
-    AUTO_NOTIFY_IGNORE+=("zsh" "fish")
-    AUTO_NOTIFY_IGNORE+=("e" "edit" "vim" "nvim" "vi" "emacs")
-fi
