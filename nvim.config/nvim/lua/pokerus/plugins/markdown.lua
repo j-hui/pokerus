957c5d529b466a6f47b5a337e36b8cc4391b2e33
j-hui <j-hui@cs.columbia.edu>
2022-12-19 03:20:49 -0500

    [nvim]: Make markdown pokerus style

--- nvim.config/nvim/lua/pokerus/plugins/markdown.lua
+++ nvim.config/nvim/lua/pokerus/plugins/markdown.lua
@@ -1,11 +1,17 @@
-return {
-  plug = function(use)
-    use {
-      "preservim/vim-markdown",
-      config = function()
-        require("pokerus").vimsetup("markdown")
-      end,
-    }
-    use "ellisonleao/glow.nvim"
-  end,
-}
+local M = {}
+
+function M.config()
+  require("pokerus").vimsetup("markdown")
+end
+
+function M.plug(use)
+  use {
+    "preservim/vim-markdown",
+    config = function()
+      require("pokerus.plugins.markdown").config()
+    end,
+  }
+  use "ellisonleao/glow.nvim"
+end
+
+return M
