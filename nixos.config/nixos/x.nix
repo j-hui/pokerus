326568e2d16baec3db548759e4f87c3375d50c60
j-hui <j-hui@cs.columbia.edu>
2022-12-24 16:02:56 -0500

    [nixos]: Use some inherits

--- nixos.config/nixos/x.nix
+++ nixos.config/nixos/x.nix
@@ -154,10 +154,11 @@ in
       dbus.enable = true;
       autorandr.enable = true;
       xserver = {
+        inherit (cfg.xkbOptions);
+
         enable = true;
         exportConfiguration = true;
         layout = "us";
-        xkbOptions = cfg.xkbOptions;
 
         videoDrivers =
           cfg.videoDrivers ++ [ "radeon" "cirrus" "vesa" "modesetting"];
@@ -234,8 +235,8 @@ in
         displayManager.lightdm = {
           enable = true;
           greeters.mini = {
+            inherit (cfg.user);
             enable = true;
-            user = cfg.user;
             extraConfig = ''
                   [greeter]
                   show-password-label = true
