7855eec6244ac55770511f269dde7b1c63cf6d95
j-hui <j-hui@cs.columbia.edu>
2023-01-24 13:39:22 -0500

    [nvim]: Made dressing.nvim VeryLazy

--- nvim.config/nvim/lua/pokerus/plugins/dressing.lua
+++ nvim.config/nvim/lua/pokerus/plugins/dressing.lua
@@ -3,6 +3,7 @@ return {
   -- dependencies = {},
   -- Dressing seems to have good enough fallbacks that I shouldn't need to
   -- require telescope or some other picker backend.
+  event = "VeryLazy",
   config = function()
     require("dressing").setup {}
   end,
