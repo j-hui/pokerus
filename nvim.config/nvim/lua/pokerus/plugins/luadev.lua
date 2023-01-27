796dbe55d7d189d4414926319e5986afec23e5f2
j-hui <j-hui@cs.columbia.edu>
2023-01-27 16:16:30 -0500

    [nvim]: Adjust luadev descriptions

--- nvim.config/nvim/lua/pokerus/plugins/luadev.lua
+++ nvim.config/nvim/lua/pokerus/plugins/luadev.lua
@@ -5,9 +5,9 @@ return {
     -- TODO: open luadev buffer automatically
     -- TODO: binding to close luadev buffer
     -- TODO: evaluate text object based on treesitter nodes
-    { "<leader>x<space>", "<cmd>Luadev<CR>", mode = "n", desc = "lua-buffer" },
-    { "<leader>xc", "<Plug>(Luadev-Run)", mode = "n", desc = "lua-run" },
-    { "<leader>xx", "<Plug>(Luadev-RunLine)", mode = "n", desc = "lua-run-line" },
-    { "<leader>xk", "<Plug>(Luadev-RunWord)", mode = "n", desc = "lua-run-word" },
+    { "<leader>x<space>", "<cmd>Luadev<CR>", mode = "n", desc = "luadev-buffer" },
+    { "<leader>xc", "<Plug>(Luadev-Run)", mode = "n", desc = "luadev-run" },
+    { "<leader>xx", "<Plug>(Luadev-RunLine)", mode = "n", desc = "luadev-run-line" },
+    { "<leader>xk", "<Plug>(Luadev-RunWord)", mode = "n", desc = "luadev-run-word" },
   },
 }
