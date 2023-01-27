c9b1280d455f4969306273172ae089bf9c9e19bc
j-hui <j-hui@cs.columbia.edu>
2023-01-27 16:15:37 -0500

    [nvim]: Set up which-key group names

--- nvim.config/nvim/lua/pokerus/plugins/which-key.lua
+++ nvim.config/nvim/lua/pokerus/plugins/which-key.lua
@@ -1,21 +1,21 @@
--- TODO: annotate prefix names
 return {
   "folke/which-key.nvim",
   opts = {
-    spelling = {
-      enabled = true,
-    },
-    hidden = {
-      "<silent>",
-      "<cmd>",
-      "<Cmd>",
-      "<CR>",
-      "call",
-      "lua",
-      "^:",
-      "^ ",
-      "<Plug>",
-      "<plug>",
-    },
-  }
+    spelling = { enabled = true, },
+    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ ", "<Plug>", "<plug>", },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register({
+      ["<leader>l"] = { name = "lsp" },
+      ["<leader>g"] = { name = "git" },
+      ["<leader>x"] = { name = "exec" },
+    }, { mode = "n" })
+    wk.register({
+      ["<leader>l"] = { name = "lsp" },
+      ["<leader>g"] = { name = "git" },
+      ["<leader>x"] = { name = "exec" },
+    }, { mode = "v" })
+  end
 }
