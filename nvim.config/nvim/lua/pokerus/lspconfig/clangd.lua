7ed868dfb8e3e0ccdccf9f76a76c29e5b2869978
j-hui <j-hui@cs.columbia.edu>
2023-02-22 14:37:04 -0500

    [nvim]: Build --query-driver param for clangd

--- nvim.config/nvim/lua/pokerus/lspconfig/clangd.lua
+++ nvim.config/nvim/lua/pokerus/lspconfig/clangd.lua
@@ -1,1 +1,21 @@
-return true
+local compilers = { "gcc", "g++", "clang" }
+local locations = {
+  "/nix/store",
+  "/usr/bin",
+  "/usr/local/bin",
+  "/opt/homebrew/bin",
+}
+
+local driver_paths = {}
+for _, c in ipairs(compilers) do
+  for _, l in ipairs(locations) do
+    table.insert(driver_paths, l .. "/*" .. c .. "*")
+  end
+end
+
+return {
+  -- TODO: Get rid of this awful config.
+  -- Tracking: https://github.com/clangd/clangd/issues/539
+  -- cmd = {"clangd", "--query-driver=/**/*"}
+  cmd = { "clangd", "--query-driver=" .. table.concat(driver_paths, ",") }
+}
