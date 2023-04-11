3124880643eec0d871b7773f36c592af21c07dcd
j-hui <j-hui@cs.columbia.edu>
2023-04-11 15:17:59 -0400

    [nvim]: Add surrounds for LaTeX commands and environments

--- nvim.config/nvim/lua/pokerus/plugins/surround.lua
+++ nvim.config/nvim/lua/pokerus/plugins/surround.lua
@@ -1,75 +1,155 @@
-local ft_opts = {
-  markdown = {
-    surrounds = {
-      -- link
-      ["l"] = {
-        add = function()
+--[[ Markdown ]] --
+
+local markdown_opts = {
+  surrounds = {
+    -- link
+    ["l"] = {
+      add = function()
+        local link = require("nvim-surround.config").get_input("Link: ")
+        return {
+          { "[" },
+          { "](" .. link .. ")" },
+        }
+      end,
+      find = "%b[]%b()",
+      delete = "^(%[)().-(%]%b())()$",
+      change = {
+        target = "^()()%b[]%((.-)()%)$",
+        replacement = function()
           local link = require("nvim-surround.config").get_input("Link: ")
           return {
-            { "[" },
-            { "](" .. link .. ")" },
+            { "" },
+            { link },
           }
         end,
-        find = "%b[]%b()",
-        delete = "^(%[)().-(%]%b())()$",
-        change = {
-          target = "^()()%b[]%((.-)()%)$",
-          replacement = function()
-            local link = require("nvim-surround.config").get_input("Link: ")
-            return {
-              { "" },
-              { link },
-            }
-          end,
-        },
       },
-      -- link text
-      ["L"] = {
-        add = function()
-          local text = require("nvim-surround.config").get_input("Text: ")
+    },
+    -- link text
+    ["L"] = {
+      add = function()
+        local text = require("nvim-surround.config").get_input("Text: ")
+        return {
+          { "[" .. text .. "](" },
+          { ")" },
+        }
+      end,
+      find = "%b[]%b()",
+      -- delete = "^(%[)().-(%]%b())()$",
+      delete = "^(%b[]%()().-(%))()$",
+      change = {
+        -- target = "^()()%b[]%((.-)()%)$",
+        target = "^%[(.-)()%]%b()()()",
+        replacement = function()
+          local link = require("nvim-surround.config").get_input("Text: ")
           return {
-            { "[" .. text .. "](" },
-            { ")" },
+            { link },
+            { "" },
           }
         end,
-        find = "%b[]%b()",
-        -- delete = "^(%[)().-(%]%b())()$",
-        delete = "^(%b[]%()().-(%))()$",
-        change = {
-          -- target = "^()()%b[]%((.-)()%)$",
-          target = "^%[(.-)()%]%b()()()",
-          replacement = function()
-            local link = require("nvim-surround.config").get_input("Text: ")
-            return {
-              { link },
-              { "" },
-            }
-          end,
-        },
+      },
+    }
+  },
+}
+
+--[[ Tex/LaTeX ]] --
+
+local function tex_find_environment()
+  local cfg = require("nvim-surround.config")
+  if vim.g.loaded_nvim_treesitter then
+    local selection = cfg.get_selection {
+      query = {
+        capture = "@block.outer",
+        type = "textobjects",
+      }
+    }
+    if selection then
+      return selection
+    end
+  end
+  return cfg.get_selection [[\begin%b{}.-\end%b{}]]
+  -- NOTE: ^this does not correctly handle \begin{}-\end{} pairs in all cases.
+end
+
+local tex_opts = {
+  surrounds = {
+    ["c"] = {
+      add = function()
+        local cfg = require("nvim-surround.config")
+        local cmd = cfg.get_input("Command: ")
+        return { { "\\" .. cmd .. "{" }, { "}" } }
+      end,
+      find = [=[\[^\{}%[%]]-%b{}]=],
+      delete = [[^(\[^\{}]-{)().-(})()$]],
+      change = {
+        target = [[^\([^\{}]-)()%b{}()()$]],
+        replacement = function()
+          local cfg = require("nvim-surround.config")
+          local cmd = cfg.get_input("Command: ")
+          return { { cmd }, { "" } }
+        end
+      },
+    },
+    ["C"] = {
+      add = function()
+        local cfg = require("nvim-surround.config")
+        local cmd, opts = cfg.get_input("Command: "), cfg.get_input("Options: ")
+        return { { "\\" .. cmd .. "[" .. opts .. "]{" }, { "}" } }
+      end,
+      find = [[\[^\{}]-%b[]%b{}]],
+      delete = [[^(\[^\{}]-%b[]{)().-(})()$]],
+      change = {
+        target = [[^\([^\{}]-)()%[(.*)()%]%b{}$]],
+        replacement = function()
+          local cfg = require("nvim-surround.config")
+          local cmd, opts = cfg.get_input("Command: "), cfg.get_input("Options: ")
+          return { { cmd }, { opts } }
+        end
+      },
+    },
+    ["e"] = {
+      add = function()
+        local cfg = require("nvim-surround.config")
+        local env = cfg.get_input("Environment: ")
+        return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
+      end,
+      find = tex_find_environment,
+      delete = [[^(\begin%b{})().*(\end%b{})()$]],
+      change = {
+        target = [[^\begin{(.-)()%}.*\end{(.-)()}$]],
+        replacement = function()
+          local env = require("nvim-surround.config").get_input("Environment: ")
+          return { { env }, { env } }
+        end,
       }
     },
-  },
-  tex = {
-    surrounds = {
-      ["c"] = {
-        add = function()
-          local cmd = require("nvim-surround.config").get_input("Command: ")
-          return { { "\\" .. cmd .. "{" }, { "}" } }
+    ["E"] = {
+      add = function()
+        local cfg = require("nvim-surround.config")
+        local env, opts = cfg.get_input("Environment: "), cfg.get_input("Options: ")
+        return { { "\\begin{" .. env .. "}[" .. opts .. "]" }, { "\\end{" .. env .. "}" } }
+      end,
+      find = tex_find_environment,
+      delete = [[^(\begin%b{}%b[])().*(\end%b{})()$]],
+      change = {
+        target = [[^\begin%b{}%[(.-)()()()%].*\end%b{}$]],
+        replacement = function()
+          local cfg = require("nvim-surround.config")
+          local env = cfg.get_input("Environment options: ")
+          return { { env }, { "" } }
         end,
-      },
-      ["e"] = {
-        add = function()
-          local env = require("nvim-surround.config").get_input("Environment: ")
-          return { { "\\begin{" .. env .. "}" }, { "\\end{" .. env .. "}" } }
-        end,
-      },
+      }
     },
   },
 }
 
+local ft_opts = {
+  markdown = markdown_opts,
+  tex = tex_opts,
+}
+
 return {
   "kylechui/nvim-surround",
-  version = "*",
+  version = "v2.0.5",
   event = "VeryLazy",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
