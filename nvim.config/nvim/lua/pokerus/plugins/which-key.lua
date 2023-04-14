182d66e765e664081616876a150a2ac72c2822e6
j-hui <j-hui@cs.columbia.edu>
2023-04-14 10:18:42 -0400

    [nvim]: Tweak whichkey operators

--- nvim.config/nvim/lua/pokerus/plugins/which-key.lua
+++ nvim.config/nvim/lua/pokerus/plugins/which-key.lua
@@ -3,6 +3,11 @@ return {
   opts = {
     spelling = { enabled = true, },
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ ", "<Plug>", "<plug>", },
+    operators = {
+      gc = "comment-line",
+      gb = "comment-block",
+      s = "substitute",
+    },
   },
   config = function(_, opts)
     local wk = require("which-key")
