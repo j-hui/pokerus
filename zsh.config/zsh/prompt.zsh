f23b696823cc5ebc5ba508691d95703ea6092fb6
John Hui <11800204+j-hui@users.noreply.github.com>
2023-08-08 18:01:29 -0700

    [zsh]: don't print vcs branch (too slow)

--- zsh.config/zsh/prompt.zsh
+++ zsh.config/zsh/prompt.zsh
@@ -43,19 +43,19 @@ function felix_pwd_abbr {
   return
 }
 
-autoload -Uz vcs_info
-vcs_precmd () { vcs_info }
-add-zsh-hook precmd vcs_precmd
-
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' unstagedstr '%F{red}*'   # display this when there are unstaged changes
-zstyle ':vcs_info:*' stagedstr '%F{yellow}+'  # display this when there are staged changes
-zstyle ':vcs_info:*' actionformats \
-    '%F{5}%F{5}[%F{2}%b%F{3}|%F{1}%a%c%u%F{5}]%f '
-zstyle ':vcs_info:*' formats       \
-    '%F{5}%F{5}[%F{2}%b%c%u%F{5}]%f '
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
-zstyle ':vcs_info:*' enable git svn
+# autoload -Uz vcs_info
+# vcs_precmd () { vcs_info }
+# add-zsh-hook precmd vcs_precmd
+#
+# zstyle ':vcs_info:*' check-for-changes true
+# zstyle ':vcs_info:*' unstagedstr '%F{red}*'   # display this when there are unstaged changes
+# zstyle ':vcs_info:*' stagedstr '%F{yellow}+'  # display this when there are staged changes
+# zstyle ':vcs_info:*' actionformats \
+#     '%F{5}%F{5}[%F{2}%b%F{3}|%F{1}%a%c%u%F{5}]%f '
+# zstyle ':vcs_info:*' formats       \
+#     '%F{5}%F{5}[%F{2}%b%c%u%F{5}]%f '
+# zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{3}%r'
+# zstyle ':vcs_info:*' enable git svn
 
 timeinfo='%F{blue}[%T]%f'
 
