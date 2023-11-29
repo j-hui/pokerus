4245e5fb2db0ded3242a0a2162a4359cbfe0b524
j-hui <j-hui@cs.columbia.edu>
2023-11-29 16:29:02 -0500

    [nvim]: tidy up telescope a little

--- nvim.config/nvim/lua/pokerus/plugins/telescope.lua
+++ nvim.config/nvim/lua/pokerus/plugins/telescope.lua
@@ -1,24 +1,3 @@
-local function action(f, ...)
-  local args = { ... }
-  return function(b)
-    require("telescope.actions")[f](b, unpack(args))
-  end
-end
-
-local function action_set(f, ...)
-  local args = { ... }
-  return function(b)
-    require("telescope.actions.set")[f](b, unpack(args))
-  end
-end
-
-local function fb_action(f, ...)
-  local args = { ... }
-  return function(b)
-    require("telescope").extensions.file_browser.actions[f](b, unpack(args))
-  end
-end
-
 local function builtin(name, opt)
   return function()
     require("telescope.builtin")[name](opt)
@@ -35,37 +14,20 @@ local function builtin_cwd(name, opt)
   opt = opt or {}
   return function()
     require("telescope.builtin")[name](
-      vim.tbl_extend(
-        "keep",
-        opt,
-        { cwd = require("telescope.utils").buffer_dir() }
-      )
-    )
+      vim.tbl_extend("keep", opt, {
+        cwd = require("telescope.utils").buffer_dir()
+      }))
   end
 end
 
-local imaps = {
-  ["<C-a>"] = { "<Home>", type = "command" },
-  ["<C-e>"] = { "<End>", type = "command" },
-  ["<C-f>"] = { "<Right>", type = "command" },
-  ["<C-b>"] = { "<Left>", type = "command" },
-  ["<C-h>"] = { "<BS>", type = "command" },
-  ["<C-k>"] = function()
-    vim.cmd [[norm! d$]]
-  end,
-  ["<Esc>"] = action "close",
-}
-
-local nmaps = {
-  ["<Up>"] = { "k", type = "command" },
-  ["<Down>"] = { "j", type = "command" },
-  ["<Left>"] = { "h", type = "command" },
-  ["<Right>"] = { "l", type = "command" },
-  ["q"] = action "close",
-  ["<C-u>"] = action_set("shift_selection", -12),
-  ["<C-d>"] = action_set("shift_selection", 12),
-  ["<C-p>"] = action_set("shift_selection", -1),
-  ["<C-n>"] = action_set("shift_selection", 1),
+-- Actions for file browsing
+local browse = {
+  folders = extension("file_browser", { files = false }),
+  folders_here = extension("file_browser", { files = false, path = "%:p:h" }),
+  browser = extension("file_browser", { files = true }),
+  browser_here = extension("file_browser", { files = true, path = "%:p:h" }),
+  files = builtin("find_files", { no_ignore = true, hidden = true }),
+  files_here = builtin("find_files", { no_ignore = true, hidden = true, cwd = "%:p:h" }),
 }
 
 return {
@@ -78,40 +40,33 @@ return {
     "debugloop/telescope-undo.nvim",
   },
   cmd = "Telescope",
+  keys = {
+    { "<leader><leader>", extension "find_pickers",                             desc = "telescope" },
+    { "<leader>f",        browse.folders,                                       desc = "telescope-folders" },
+    { "<leader>F",        browse.folders_here,                                  desc = "telescope-folders-." },
+    { "<leader>e",        browse.browser,                                       desc = "telescope-browser" },
+    { "<leader>E",        browse.browser_here,                                  desc = "telescope-browser-." },
+    { "<leader>o",        browse.files,                                         desc = "telescope-files" },
+    { "<leader>O",        browse.files_here,                                    desc = "telescope-files-." },
+    { "<leader>b",        builtin "buffers",                                    desc = "telescope-buffers" },
+    { "<leader>*",        builtin "grep_string",                                desc = "telescope-grep-word" },
+    { "<leader>r",        builtin_cwd "live_grep",                              desc = "telescope-grep-live" },
+    { "<leader>/",        builtin_cwd("live_grep", { grep_open_files = true }), desc = "telescope-grep-buf" },
+    { "<leader>h",        builtin "help_tags",                                  desc = "telescope-help" },
+    { "<leader>c",        builtin "commands",                                   desc = "telescope-commands" },
+    { "<leader>K",        builtin "man_pages",                                  desc = "telescope-man-pages" },
+    { "<leader>u",        extension "undo",                                     desc = "telescope-undo" },
+    { "<leader>gf",       builtin "git_files",                                  desc = "git-files" },
+    { "<leader>gm",       builtin "git_status",                                 desc = "git-modified" },
+    { "<leader>gl",       builtin "git_commits",                                desc = "git-log" },
+    { "<leader>gL",       builtin "git_bcommits",                               desc = "git-buffer-log" },
+    { "<leader>gb",       builtin "git_branches",                               desc = "git-branches" },
+    { "<leader>ld",       builtin "diagnostics",                                desc = "telescope-diagnostics" },
+    { "<leader>l?",       builtin "lsp_workspace_symbols",                      desc = "lsp-workspace-symbols" },
+    { "<leader>l/",       builtin "lsp_document_symbols",                       desc = "lsp-document-symbols" },
+  },
+
   init = function()
-    local function nmap(...)
-      vim.keymap.set("n", ...)
-    end
-
-    nmap("<leader><leader>", extension "find_pickers", { desc = "telescope" })
-
-    nmap("<leader>f", extension("file_browser", {}), { desc = "telescope-file-browser" })
-    nmap("<leader>E", extension("file_browser", { respect_gitignore = false }), { desc = "telescope-file-browser*" })
-    nmap("<leader>e", extension("file_browser", { path = "%:p:h" }), { desc = "telescope-edit" })
-    nmap("<leader>E", extension("file_browser", { path = "%:p:h", respect_gitignore = false }),
-      { desc = "telescope-edit*" })
-    nmap("<leader>o", builtin "find_files", { desc = "telescope-files" })
-    nmap("<leader>O", builtin("find_files", { no_ignore = true }), { desc = "telescope-files*" })
-
-    nmap("<leader>b", builtin "buffers", { desc = "telescope-buffers" })
-    nmap("<leader>*", builtin "grep_string", { desc = "telescope-grep-word" })
-    nmap("<leader>r", builtin_cwd "live_grep", { desc = "telescope-grep-live" })
-    nmap("<leader>/", builtin_cwd("live_grep", { grep_open_files = true }), { desc = "telescope-sneak" })
-    nmap("<leader>h", builtin "help_tags", { desc = "telescope-help" })
-    nmap("<leader>c", builtin "commands", { desc = "telescope-commands" })
-    nmap("<leader>K", builtin "man_pages", { desc = "telescope-man-pages" })
-    nmap("<leader>u", extension "undo", { desc = "telescope-undo" })
-
-    nmap("<leader>gf", builtin "git_files", { desc = "git-files" })
-    nmap("<leader>gm", builtin "git_status", { desc = "git-modified" })
-    nmap("<leader>gl", builtin "git_commits", { desc = "git-log" })
-    nmap("<leader>gL", builtin "git_bcommits", { desc = "git-buffer-log" })
-    nmap("<leader>gb", builtin "git_branches", { desc = "git-branches" })
-
-    nmap("<leader>ld", builtin "diagnostics", { desc = "telescope-diagnostics" })
-    nmap("<leader>l?", builtin "lsp_workspace_symbols", { desc = "lsp-workspace-symbols" })
-    nmap("<leader>l/", builtin "lsp_document_symbols", { desc = "lsp-document-symbols" })
-
     -- Override some of my own lsp handlers
     local lsp = require("pokerus.lsp")
 
@@ -146,10 +101,54 @@ return {
     cmd("Rg", function(t)
       require("telescope.builtin").grep_string { search = t.args }
     end, { desc = "telescope-grep", nargs = "+" })
-
   end,
+
   config = function()
-    require("telescope").setup {
+    local telescope = require "telescope"
+    local action = require "telescope.actions"
+    local fb_action = telescope.extensions.file_browser.actions
+    local function action_set(f, ...)
+      local args = { ... }
+      return function(b)
+        require("telescope.actions.set")[f](b, unpack(args))
+      end
+    end
+
+    local imaps = {
+      ["<C-a>"] = { "<Home>", type = "command" },
+      ["<C-e>"] = { "<End>", type = "command" },
+      ["<C-f>"] = { "<Right>", type = "command" },
+      ["<C-b>"] = { "<Left>", type = "command" },
+      ["<C-h>"] = { "<BS>", type = "command" },
+      ["<C-k>"] = { "<cmd>norm! d$<cr>", type = "command" },
+      ["<Esc>"] = action.close,
+      ["<C-]>"] = { "<Esc>", type = "command" },
+      -- NOTE: scroll = half-page, shift = single entry
+      ["<C-u>"] = action_set("scroll_results", -1),
+      ["<C-d>"] = action_set("scroll_results", 1),
+      ["<C-p>"] = action_set("shift_selection", -1),
+      ["<C-n>"] = action_set("shift_selection", 1),
+      ["<M-u>"] = action_set("scroll_previewer", -1),
+      ["<M-d>"] = action_set("scroll_previewer", 1),
+      ["<C-x>"] = action.which_key,
+    }
+
+    local nmaps = {
+      ["<Up>"] = { "k", type = "command" },
+      ["<Down>"] = { "j", type = "command" },
+      ["<Left>"] = { "h", type = "command" },
+      ["<Right>"] = { "l", type = "command" },
+      ["q"] = action.close,
+      ["<C-u>"] = action_set("scroll_results", -1),
+      ["<C-d>"] = action_set("scroll_results", 1),
+      ["<C-p>"] = action_set("shift_selection", -1),
+      ["<C-n>"] = action_set("shift_selection", 1),
+      ["<M-u>"] = action_set("scroll_previewer", -1),
+      ["<M-d>"] = action_set("scroll_previewer", 1),
+    }
+
+
+    telescope.setup {
       defaults = require("telescope.themes").get_ivy {
         winblend = 20,
         mappings = { i = imaps, n = nmaps },
@@ -167,31 +166,44 @@ return {
       },
       pickers = {
         find_files = {
-          hidden = true,
+          follow = true, -- follow symlinks
         },
       },
       extensions = {
         file_browser = {
-          hidden = true, -- show hidden files
-          grouped = true, -- group folders together
-          cwd_to_path = true,
+          hidden = true,      -- show hidden files
+          grouped = true,     -- group folders together
+          cwd_to_path = true, -- use path as cwd
+          follow_symlinks = true,
+          collapse_dirs = true,
           mappings = {
             n = nmaps,
             i = vim.tbl_extend("force", imaps, {
+              ["<C-w>"] = { "<cmd>norm! db<cr>", type = "command" },
+              ["<S-CR>"] = fb_action.create_from_prompt,
+              ["<BS>"] = fb_action.backspace,
+
               ["<C-g>"] = false,
-              ["<C-o>"] = fb_action "create",
-              ["<C-l>"] = fb_action "toggle_browser",
-              ["<C-t>"] = fb_action "toggle_hidden",
-              ["<C-y>"] = fb_action "goto_cwd",
-              ["<C-]>"] = fb_action "change_cwd",
-              ["<C-r>"] = fb_action "rename",
-              ["<C-s>"] = fb_action "move",
-              ["<C-c>"] = fb_action "copy",
-              ["<C-x>"] = fb_action "remove",
-              ["~"] = fb_action "goto_home_dir",
-              ["<C-w>"] = function()
-                vim.cmd [[norm! db]]
-              end,
+
+              ["<C-g>o"] = fb_action.create,
+              ["<C-g><CR>"] = fb_action.create_from_prompt,
+              ["<C-g>r"] = fb_action.rename,
+              ["<C-g>x"] = fb_action.remove,
+              ["<C-g>m"] = fb_action.move, -- move selected to here
+              ["<C-g>y"] = fb_action.copy, -- copy selected to here
+
+              ["<C-g><C-g>"] = fb_action.toggle_browser,
+              ["<C-g>h"] = fb_action.toggle_hidden,
+              ["<C-g>i"] = fb_action.toggle_respect_gitignore,
+
+              ["<C-g>-"] = fb_action.goto_parent_dir,
+              ["<C-g>="] = fb_action.goto_cwd,
+              ["<C-g>/"] = fb_action.goto_home_dir,
+              ["<C-g>."] = fb_action.change_cwd,
+
+              ["<C-g>a"] = fb_action.select_all,
+              ["<C-g>d"] = fb_action.sort_by_date,
+              ["<C-g>s"] = fb_action.sort_by_size,
             }),
           },
         },
@@ -200,8 +212,8 @@ return {
         },
       },
     }
-    require("telescope").load_extension "fzf"
-    require("telescope").load_extension "file_browser"
-    require("telescope").load_extension("undo")
+    telescope.load_extension "fzf"
+    telescope.load_extension "file_browser"
+    telescope.load_extension "undo"
   end,
 }
