ef4892d4923460792fa865ae3d0eedcebdaeb8ea
j-hui <j-hui@cs.columbia.edu>
2024-08-15 23:52:46 -0400

    [nvim]: Add oil

---
+++ nvim.config/nvim/lua/pokerus/plugins/oil.lua
@@ -0,0 +1,9 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- Can also use "echasnovski/mini.icons" but I'm already using nvim-web-devicons elsewhere
+  opts = {
+    view_options = {
+      show_hidden = true,
+    }
+  },
+}
