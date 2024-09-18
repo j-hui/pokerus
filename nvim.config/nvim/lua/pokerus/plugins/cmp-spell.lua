c2074611d766e385373e66bd6c8f35b5b1ea0c22
j-hui <j-hui@cs.columbia.edu>
2024-09-18 08:29:44 -0700

    [nvim]: fix (?) cmp-spell configuration

--- nvim.config/nvim/lua/pokerus/plugins/cmp-spell.lua
+++ nvim.config/nvim/lua/pokerus/plugins/cmp-spell.lua
@@ -1,18 +1,16 @@
 return {
   "f3fora/cmp-spell",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-      "nvim-treesitter/nvim-treesitter",
-    },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+    "nvim-treesitter/nvim-treesitter",
+  },
   init = function()
     table.insert(require("pokerus.plugins.cmp").sources, {
-      {
-        name = "spell",
-        option = {
-          enable_in_context = function(params)
-            return require('cmp.config.context').in_treesitter_capture('spell')
-          end,
-        }
+      name = "spell",
+      option = {
+        enable_in_context = function(params)
+          return require('cmp.config.context').in_treesitter_capture('spell')
+        end,
       }
     })
   end,
