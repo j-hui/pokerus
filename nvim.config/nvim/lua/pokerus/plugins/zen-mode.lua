0b64f4a29602acc7835afbc9ec1a15625dd1dcd0
j-hui <j-hui@cs.columbia.edu>
2024-09-18 08:06:31 -0700

    [nvim]: tweak zen mode

--- nvim.config/nvim/lua/pokerus/plugins/zen-mode.lua
+++ nvim.config/nvim/lua/pokerus/plugins/zen-mode.lua
@@ -1,10 +1,30 @@
 return {
-  "folke/zen-mode.nvim",
-  cmd = { "ZenMode" },
-  opts = {
-    twilight = { enabled = true },
+  {
+    "folke/zen-mode.nvim",
+    dependencies = { "folke/twilight.nvim" },
+    cmd = { "ZenMode" },
+    opts = {
+      twilight = { enabled = true },
+    },
+    keys = {
+      { "<leader>T", mode = { "n" }, "<cmd>ZenMode<cr>", desc = "zen-mode" }
+    },
   },
-  keys = {
-    { "<leader>G", function() require("zen-mode").toggle() end, desc = "zen-mode" },
-  },
+  {
+    "folke/twilight.nvim",
+    cmd = { "Twilight", "TwilightEnable", "TwilightDisable" },
+    keys = {
+      { "<leader>t", mode = { "n" }, "<cmd>Twilight<cr>", desc = "twilight" }
+    },
+    opts = {
+      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+        "function",
+        "method",
+        "table",
+        "if_statement",
+        "section",
+      },
+      exclude = {}, -- exclude these filetypes
+    }
+  }
 }
