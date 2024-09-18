8c37743e26f44863802e29a92b9fe8fcfe3a6a97
j-hui <j-hui@cs.columbia.edu>
2024-09-18 08:14:48 -0700

    [nvim]: fix conform configuration format

--- nvim.config/nvim/lua/pokerus/plugins/conform.lua
+++ nvim.config/nvim/lua/pokerus/plugins/conform.lua
@@ -20,6 +20,23 @@ local function do_format()
   })
 end
 
+--- Choose first available formatter.
+---
+--- From: https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#run-the-first-available-formatter-followed-by-more-formatters
+---@param bufnr integer
+---@param ... string
+---@return string
+local function first(bufnr, ...)
+  local conform = require("conform")
+  for i = 1, select("#", ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
+
 return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
@@ -32,13 +49,21 @@ return {
     formatters_by_ft = {
       c = {},
       cpp = {},
-      lua = { "stylua" },
-      javascript = { { "prettierd", "prettier" } },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
       json = { "jq" },
-      yaml = { "yq" },
-      toml = { "taplo" },
-      rust = { "rustfmt" },
+      lua = { "stylua" },
+      markdown = function(bufnr)
+        return {
+          -- first(bufnr, "markdownlint"),
+          "markdownlint",
+          "mdslw",
+          "injected",
+        }
+      end,
       python = { "isort", "black" },
+      rust = { "rustfmt" },
+      toml = { "taplo" },
+      yaml = { "yq" },
     },
   },
   init = function()
