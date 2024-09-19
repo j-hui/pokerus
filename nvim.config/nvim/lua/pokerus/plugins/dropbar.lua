4103a305df12536831c521b42e05f954e25562c2
John Hui <j-hui@apple.com>
2024-09-19 16:35:18 -0700

    [nvim]: switch from navbuddy to dropbar

---
+++ nvim.config/nvim/lua/pokerus/plugins/dropbar.lua
@@ -0,0 +1,8 @@
+return {
+  "Bekaboo/dropbar.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter",
+  }
+}
