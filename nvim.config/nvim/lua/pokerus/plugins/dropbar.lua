2f05b05f33085af4536ae8bdec90da55a39add73
John Hui <j-hui@apple.com>
2024-09-30 15:59:03 -0700

    nvim: disable dropbar's own lazy loader

--- nvim.config/nvim/lua/pokerus/plugins/dropbar.lua
+++ nvim.config/nvim/lua/pokerus/plugins/dropbar.lua
@@ -4,5 +4,12 @@ return {
     "nvim-telescope/telescope-fzf-native.nvim",
     "nvim-tree/nvim-web-devicons",
     "nvim-treesitter/nvim-treesitter",
-  }
+  },
+
+  event = "VeryLazy",
+  init = function()
+    -- Disable dropbar's own lazy loader
+    vim.g.loaded_dropbar = true
+  end,
+  opts = {},
 }
