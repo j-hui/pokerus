b29bf1119e72dc9d56b377c92d6f4beb15c57f75
John Hui <j-hui@apple.com>
2024-10-03 10:53:21 -0700

    nvim: adjust Trouble settings

--- nvim.config/nvim/lua/pokerus/plugins/trouble.lua
+++ nvim.config/nvim/lua/pokerus/plugins/trouble.lua
@@ -1,25 +1,31 @@
--- NOTE: supersedes quicker.nvim, aerial.nvim, and several aspects of Telescope
 return {
   "folke/trouble.nvim",
+  -- NOTE: supersedes quicker.nvim, aerial.nvim, and several aspects of Telescope
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {},
+  opts = {
+    auto_preview = false, -- Auto preview slows everything down. Instead, use `p` to preview in the Trouble pane.
+    throttle = {
+      follow = { ms = 500, debounce = true },
+      preview = { ms = 500, debounce = true },
+    },
+  },
   cmd = "Trouble",
   keys = {
-    { "gkq", mode = "n", "<cmd>Trouble qflist<cr>",               desc = "quickfix-list" },
-    { "gkl", mode = "n", "<cmd>Trouble loclist<cr>",              desc = "location-list" },
-    { "gks", mode = "n", "<cmd>Trouble symbols<cr>",              desc = "symbols" },
-    { "gkt", mode = "n", "<cmd>Trouble todo<cr>",                 desc = "todos" },
-    { "gkd", mode = "n", "<cmd>Trouble diagnostics<cr>",          desc = "diagnostics" },
+    { "gkq", mode = "n", "<cmd>Trouble qflist toggle<cr>",               desc = "quickfix-list" },
+    { "gkl", mode = "n", "<cmd>Trouble loclist toggle<cr>",              desc = "location-list" },
+    { "gks", mode = "n", "<cmd>Trouble symbols toggle<cr>",              desc = "symbols" },
+    { "gkt", mode = "n", "<cmd>Trouble todo toggle<cr>",                 desc = "todos" },
+    { "gkd", mode = "n", "<cmd>Trouble diagnostics toggle<cr>",          desc = "diagnostics" },
 
-    { "gll", mode = "n", "<cmd>Trouble lsp<cr>",                  desc = "lsp-all" },
-    { "gls", mode = "n", "<cmd>Trouble lsp_document_symbols<cr>", desc = "lsp-symbols" },
-    { "gld", mode = "n", "<cmd>Trouble lsp_definitions<cr>",      desc = "lsp-definitions" },
-    { "glr", mode = "n", "<cmd>Trouble lsp_references<cr>",       desc = "lsp-references" },
-    { "glc", mode = "n", "<cmd>Trouble lsp_command<cr>",          desc = "lsp-commands" },
-    { "gln", mode = "n", "<cmd>Trouble lsp_declarations<cr>",     desc = "lsp-declarations" },
-    { "glm", mode = "n", "<cmd>Trouble lsp_implementations<cr>",  desc = "lsp-implementations" },
-    { "gli", mode = "n", "<cmd>Trouble lsp_incoming_calls<cr>",   desc = "lsp-incoming-calls" },
-    { "glo", mode = "n", "<cmd>Trouble lsp_outgoing_calls<cr>",   desc = "lsp-outgoing-calls" },
-    { "glt", mode = "n", "<cmd>Trouble lsp_type_definitions<cr>", desc = "lsp-type-definitions" },
+    { "gll", mode = "n", "<cmd>Trouble lsp toggle<cr>",                  desc = "lsp-all" },
+    { "gls", mode = "n", "<cmd>Trouble lsp_document_symbols toggle<cr>", desc = "lsp-symbols" },
+    { "gld", mode = "n", "<cmd>Trouble lsp_definitions toggle<cr>",      desc = "lsp-definitions" },
+    { "glr", mode = "n", "<cmd>Trouble lsp_references toggle<cr>",       desc = "lsp-references" },
+    { "glc", mode = "n", "<cmd>Trouble lsp_command toggle<cr>",          desc = "lsp-commands" },
+    { "gln", mode = "n", "<cmd>Trouble lsp_declarations toggle<cr>",     desc = "lsp-declarations" },
+    { "glm", mode = "n", "<cmd>Trouble lsp_implementations toggle<cr>",  desc = "lsp-implementations" },
+    { "gli", mode = "n", "<cmd>Trouble lsp_incoming_calls toggle<cr>",   desc = "lsp-incoming-calls" },
+    { "glo", mode = "n", "<cmd>Trouble lsp_outgoing_calls toggle<cr>",   desc = "lsp-outgoing-calls" },
+    { "glt", mode = "n", "<cmd>Trouble lsp_type_definitions toggle<cr>", desc = "lsp-type-definitions" },
   },
 }
