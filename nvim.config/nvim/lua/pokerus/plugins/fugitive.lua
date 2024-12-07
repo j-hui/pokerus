2edc1074c905c6741fc44787895a559fea09f966
John Hui <j-hui@apple.com>
2024-12-06 20:33:23 -0800

    nvim: delete fugitive bindings
    Switching to use neogit now, but I will keep fugitive around for the
    convenient ex-mode command

--- nvim.config/nvim/lua/pokerus/plugins/fugitive.lua
+++ nvim.config/nvim/lua/pokerus/plugins/fugitive.lua
@@ -2,16 +2,6 @@ return {
   "tpope/vim-fugitive",
   event = "VeryLazy",
   cmd = { "G", "Git" },
-  keys = {
-    -- { "<leader>gd", "<cmd>Gdiffsplit<CR>", mode = "n", desc = "git-diff-split" },
-    -- { "<leader>gD", "<cmd>Git diff --cached<CR>", mode = "n", desc = "git-diff-cached" },
-    { "<leader>gp", "<cmd>Git pull<CR>", mode = "n", desc = "git-pull" },
-    { "<leader>gP", "<cmd>Git push<CR>", mode = "n", desc = "git-push" },
-    { "<leader>gc", "<cmd>Git commit<CR>", mode = "n", desc = "git-commit" },
-    { "<leader>gC", "<cmd>Git commit --amend<CR>", mode = "n", desc = "git-amend" },
-    -- { "<leader>gw", "<cmd>Gwrite<CR>", mode = "n", desc = "git-write" },
-    { "<leader>gs", "<cmd>Git<CR>", mode = "n", desc = "git-status" },
-  },
   config = function()
     require("pokerus").vimsetup("fugitive")
   end,
