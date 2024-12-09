ff081c296b402687de498d84830672016dd187f9
John Hui <j-hui@apple.com>
2024-12-09 10:56:04 -0800

    nvim: configure scrollbar + integrate with gitsigns

--- nvim.config/nvim/lua/pokerus/plugins/scrollbar.lua
+++ nvim.config/nvim/lua/pokerus/plugins/scrollbar.lua
@@ -1,4 +1,20 @@
 return {
   "petertriho/nvim-scrollbar",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",
+  },
+  event = "VeryLazy",
   config = true,
+  opt = {
+    throttle_ms = 1000,
+    hide_if_all_visible = true,
+    show_in_active_only = true,
+    folds = 1000,      -- (false|number) don't handle folds if line count exceeds this
+    max_lines = false, -- (false|number) don't render if line count exceeds this
+    handlers = {
+      cursor = true,
+      diagnostic = true,
+      gitsigns = true,
+    },
+  },
 }
