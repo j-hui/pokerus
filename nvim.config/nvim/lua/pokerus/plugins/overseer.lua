731c579d2b70fe76342201dee22d5061e09baaf5
John Hui <j-hui@apple.com>
2024-12-09 10:48:20 -0800

    nvim: add overseer bindings

--- nvim.config/nvim/lua/pokerus/plugins/overseer.lua
+++ nvim.config/nvim/lua/pokerus/plugins/overseer.lua
@@ -1,11 +1,12 @@
 return {
   "stevearc/overseer.nvim",
+  event = "VeryLazy",
   opts = {
   },
   keys = {
-    { "gj",               "<cmd>OverseerToggle<CR>",  desc = "overseer-open" },
-    { "gJ",               "<cmd>OverseerToggle!<CR>", desc = "overseer-open!" },
-    { "<leader>j<space>", "<cmd>OverseerCmdRun<CR>",  desc = "overseer-cmd" },
-    { "<leader>jj",       "<cmd>OverseerRun<CR>",     desc = "overseer-run" },
+    { "gj",         "<cmd>OverseerToggle<CR>",  desc = "overseer-open" },
+    { "gJ",         "<cmd>OverseerToggle!<CR>", desc = "overseer-open!" },
+    { "<leader>jc", "<cmd>OverseerRunCmd<CR>",  desc = "overseer-cmd" },
+    { "<leader>jr", "<cmd>OverseerRun<CR>",     desc = "overseer-run" },
   },
 }
